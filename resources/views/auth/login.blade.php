b5cdbb314db5a2c3e9b5461b93927abf2ca75589
Nikspatel22994 <assertivlogix@gmail.com>
2026-01-11 22:42:39 +0530

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.frontend')
 
 @section('content')
+<style type="text/css">
+.addmargin{
+    margin: 50px 0px;
+}
+</style>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card addmargin">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
